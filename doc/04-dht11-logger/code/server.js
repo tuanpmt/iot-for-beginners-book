7ac5770018831e82c09f7e95ead8a8cccd3bf632
Tuan PM <tuanpm@live.com>
2017-08-22 00:08:15 +0700

    update dht11 logger section & fix some typo

--- doc/04-dht11-logger/code/server.js
+++ doc/04-dht11-logger/code/server.js
@@ -1,19 +1,38 @@
-var http = require('http');
+var fs = require('fs');
 var url = require('url');
+var http = require('http');
 var querystring = require('querystring');
+var db = []; //database
 
-//Query: http://localhost:8000/update?temp=30.5&hum=80
 function requestHandler(request, response) {
   var uriData = url.parse(request.url);
   var pathname = uriData.pathname;          // /update
   var query = uriData.query;                // temp=30.5&hum=80
   var queryData = querystring.parse(query); // queryData.temp = 30.5
-                                            // queryData.hum = 80
+
   if(pathname == '/update') {
-    console.log(queryData);
+    var newData = {
+        temp: queryData.temp,
+        humd: queryData.humd,
+        time: new Date()        //current time
+    }
+    db.push(newData);
+    console.log(newData);
+    response.end();             // return status 200 OK
+  } else if(pathname == '/get') {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(db));
+    db = [];
+  } else {
+    fs.readFile('./index.html', function(error, content) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(content);
+    });
+
   }
-  response.end('ok')
+
 }
+
 var server = http.createServer(requestHandler);
 server.listen(8000);
 console.log('Server listening on port 8000');
