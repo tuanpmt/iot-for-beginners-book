cf5ac949d3d58b6962604c4aae87a76931e0d06f
Tuan PM <tuanpm@live.com>
2017-09-03 17:46:24 +0700

    add httpUpdate with nodejs server

--- doc/08-fota/code/httpUpdate/httpUpdate.ino
+++ doc/08-fota/code/httpUpdate/httpUpdate.ino
@@ -3,19 +3,24 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
 
-const char* ssid = "...";
-const char* password = "...";
-const char* server_file = "https://server/file.bin";
-
+const char* ssid = "TuanPM";
+const char* password = "mobileday";
+const char *currentVersion = "1.0"; //<1>
+const char *serverUrl = "http://192.168.0.106:8000/firmware.bin"; //<2>
 void setup() {
 
   Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  Serial.print("ESP8266 http update, current version: ");
+  Serial.println(currentVersion);
+  
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  t_httpUpdate_return ret = ESPhttpUpdate.update(server_file);
+  t_httpUpdate_return ret = ESPhttpUpdate.update(serverUrl, currentVersion);
   switch (ret) {
     case HTTP_UPDATE_FAILED:
       Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
